31d0db87ee142d00c0383c5d1801d785506e87b2
FANGZx <1724850609@qq.com>
2026-05-22 02:31:33 +0800

    chore: bump version to 1.4.0 (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.opencode.remote"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.3.1"
+        versionCode = 7
+        versionName = "1.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
